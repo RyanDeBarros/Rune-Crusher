47c060f6abc84877caa2f23771e5b83d868cc5fd
RyanDeBarros <ryan2004db@gmail.com>
2025-10-02 14:42:03 -0400

    Level HUD can set score, timer, and candies left texts

--- Assets/Scripts/UI/LevelHUDController.cs
+++ Assets/Scripts/UI/LevelHUDController.cs
@@ -2,30 +2,91 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+using TMPro;
 
 public class LevelHUDController : MonoBehaviour
 {
     [SerializeField] private GameObject pauseCanvas;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI candiesLeftText;
+    [SerializeField] private TextMeshProUGUI timeRemainingText;
+
+    [SerializeField] private int candiesLeft = 100;
+    [SerializeField] private int timeRemaining = 100;
+
+    private bool isPlaying = true;
+    private float timeRemainingFloat = 0f;
+
+    private void Awake()
+    {
+        Assert.IsNotNull(pauseCanvas);
+        pauseCanvas.SetActive(false);
+
+        if (pauseCanvas.TryGetComponent(out Image image))
+        {
+            image.enabled = true;
+        }
+
+        Assert.IsNotNull(scoreText);
+        Assert.IsNotNull(candiesLeftText);
+        Assert.IsNotNull(timeRemainingText);
+    }
 
     private void Start()
     {
-        pauseCanvas.SetActive(false);
+        SetNumberOfCandiesLeftText();
+        timeRemainingFloat = timeRemaining;
+        SetTimeRemainingText();
+    }
+
+    private void Update()
+    {
+        if (isPlaying)
+        {
+            timeRemainingFloat -= Time.deltaTime;
+            // TODO if remainingTextFloat <= 0 -> game over
+            timeRemaining = (int)timeRemainingFloat;
+            SetTimeRemainingText();
+        }
     }
 
     public void OnPauseButtonClicked()
     {
-        // TODO pause game and timer
         pauseCanvas.SetActive(true);
+        isPlaying = false;
     }
 
     public void OnResumeButtonClicked()
     {
-        // TODO resume game and timer
         pauseCanvas.SetActive(false);
+        isPlaying = true;
     }
 
     public void OnQuitButtonClicked()
     {
         SceneManager.LoadScene((int)SceneList.MainMenu);
+    }
+
+    public void SetScoreText(int score)
+    {
+        scoreText.SetText($"Score: {score}");
+    }
+
+    public void DecrementCandiesLeft(int decrement)
+    {
+        candiesLeft -= decrement;
+        SetNumberOfCandiesLeftText();
+    }
+
+    private void SetNumberOfCandiesLeftText()
+    {
+        candiesLeftText.SetText($"Candies left: {candiesLeft}");
+    }
+
+    private void SetTimeRemainingText()
+    {
+        timeRemainingText.SetText($"Time left: {timeRemaining}");
     }
 }
