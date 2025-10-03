1c71fae9d3ea290216eaf61ce74c4a6f47b02232
RyanDeBarros <ryan2004db@gmail.com>
2025-10-03 19:07:05 -0400

    Start textures in level complete HUD displayed based on score

--- Assets/Scripts/UI/LevelCompleteHUDController.cs
+++ Assets/Scripts/UI/LevelCompleteHUDController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using TMPro;
 
 public class LevelCompleteHUDController : MonoBehaviour
@@ -10,12 +11,25 @@ public class LevelCompleteHUDController : MonoBehaviour
     [SerializeField] private TextMeshProUGUI levelNameText;
     [SerializeField] private TextMeshProUGUI scoreText;
 
+    [SerializeField] private RawImage star1, star2, star3;
+    [SerializeField] private Texture starTexture, noStarTexture;
+
+    // TODO playtest thresholds
+    [SerializeField] private int oneStarScoreThreshold = 100, twoStarsScoreThreshold = 500, threeStarsScoreThreshold = 1000;
+
     public string levelName;
 
     private void Awake()
     {
         Assert.IsNotNull(levelNameText);
         Assert.IsNotNull(scoreText);
+
+        Assert.IsNotNull(star1);
+        Assert.IsNotNull(star2);
+        Assert.IsNotNull(star3);
+
+        Assert.IsNotNull(starTexture);
+        Assert.IsNotNull(noStarTexture);
     }
 
     private void Start()
@@ -26,7 +40,9 @@ public class LevelCompleteHUDController : MonoBehaviour
     public void SetScore(int score)
     {
         scoreText.SetText($"Score: {score}");
-        // TODO display stars based on score
+        star1.texture = score >= oneStarScoreThreshold ? starTexture : noStarTexture;
+        star2.texture = score >= twoStarsScoreThreshold ? starTexture : noStarTexture;
+        star3.texture = score >= threeStarsScoreThreshold ? starTexture : noStarTexture;
     }
 
     public void OnContinueButtonClicked()
