0eb527f204de5a3eea9b0762859876c79e0b0181
RyanDeBarros <ryan2004db@gmail.com>
2025-10-04 00:26:22 -0400

    Added TODOs for the rest of tasks

--- Assets/Scripts/ScoreTracker.cs
+++ Assets/Scripts/ScoreTracker.cs
@@ -9,7 +9,15 @@ public class ScoreTracker : MonoBehaviour
     [SerializeField] private LevelHUDController hud;
     [SerializeField] private int scorePerMatch = 10;
     [SerializeField] private float cascadeMultiplier = 2f;
-    [SerializeField] private int initialRunesLeft = 20;
+    [SerializeField] private int initialRunesLeft = 20; // TODO there should not be a target rune - must match 3 combos of each color. A combo is a match of 3+ runes. Re-playtest for proper star score thresholds.
+
+    // TODO add restart button in pause menu
+    // TODO disallow swap if it doesn't result in a match - do animation for this.
+    // TODO if no possible moves left, reshuffle the runes (call FillEntireGrid() until moves are possible).
+    // TODO SFX
+    // TODO music
+    // TODO spice up UI / background
+    // TODO bonus: scoring VFX
 
     private RuneColor targetRune;
     private int score;
